143cef5c6fa02d731c70b84b62fc81447552b276
Christian Mahnke <cmahnke@gmail.com>
2024-09-08 07:34:57 +0200

    Fix formating

--- src/hdr-check.ts
+++ src/hdr-check.ts
@@ -49,10 +49,12 @@ export function checkHDRCanvas(): boolean {
       return false;
     }
     return true;
-  /* eslint-disable no-console, @typescript-eslint/no-unused-vars */
+    /* eslint-disable no-console, @typescript-eslint/no-unused-vars */
   } catch (e) {
     //console.error("Bad canvas ColorSpace test", e);
-    console.error("Bad canvas ColorSpace test - make sure that the Chromium browser flag 'enable-experimental-web-platform-features' has been enabled");
+    console.error(
+      "Bad canvas ColorSpace test - make sure that the Chromium browser flag 'enable-experimental-web-platform-features' has been enabled",
+    );
 
     return false;
   }
