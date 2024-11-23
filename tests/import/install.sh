077f7bb03e6feaf914d3791c01eb08a147516db0
Christian Mahnke <cmahnke@gmail.com>
2024-11-23 12:33:38 +0100

    Try to fix postinstall

--- tests/import/install.sh
+++ tests/import/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+#export NODE_OPTIONS="--require=suppress-experimental-warnings --experimental-loader=node-esm-loader"
+rm -rf node_modules package-lock.json
 npm i --no-package-lock $1 --prefix ./
