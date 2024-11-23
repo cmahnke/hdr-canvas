ea89d1b901204fbe2411935a402b66ef0b78221a
Christian Mahnke <cmahnke@gmail.com>
2024-11-23 09:48:20 +0100

    Provide import test

---
+++ tests/import/import.ts
@@ -0,0 +1,9 @@
+import { Uint16Image, checkHDR, checkHDRCanvas, initHDRCanvas, defaultGetContextHDR, resetGetContext } from "hdr-canvas";
+import HDRWebGPUBackend from 'hdr-canvas/three/HDRWebGPUBackend.js';
+import HDRWebGPURenderer from 'hdr-canvas/three/HDRWebGPURenderer.js';
+
+window.checkHDR = checkHDR;
+window.checkHDRCanvas = checkHDRCanvas;
+window.initHDRCanvas = initHDRCanvas;
+window.defaultGetContextHDR = defaultGetContextHDR;
+window.resetGetContext = resetGetContext;
