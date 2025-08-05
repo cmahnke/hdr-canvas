62691d0780559d410ee3b2878fc052df97390e87
cmahnke <194820+cmahnke@users.noreply.github.com>
2025-08-05 09:10:57 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,8 @@ export default [
       ...tseslint.configs.recommended.rules,
       "no-warning-comments": ["warn", {}],
       "@typescript-eslint/no-unused-vars": ["warn"],
-      "@typescript-eslint/no-explicit-any": ["warn"]
+      "@typescript-eslint/no-explicit-any": ["warn"],
+      "@typescript-eslint/no-empty-object-type": ["warn"]
     }
   },
   {
